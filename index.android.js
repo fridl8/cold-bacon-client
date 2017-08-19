8fd5294ee9dc6a405758a39d3b48bbd5def37045
fridl8 <tdf970@gmail.com>
2017-08-19 17:44:50 -0500

    Add missing semicolon in android index

--- index.android.js
+++ index.android.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
 import { AppRegistry } from 'react-native';
-import LaunchScreen from './containers/LaunchScreen'
+import LaunchScreen from './containers/LaunchScreen';
 
 AppRegistry.registerComponent('client', () => LaunchScreen);
