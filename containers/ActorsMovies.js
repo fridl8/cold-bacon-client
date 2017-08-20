f57f9350d9fa7e2844b82998821cf50fcdbbc5dd
Tyler Fridley <email": null>
2017-08-20 11:46:35 -0500

    Change return text

--- containers/ActorsMovies.js
+++ containers/ActorsMovies.js
@@ -7,7 +7,7 @@ import { StackNagivator } from 'react-navigation';
 export default class ActorsMovies extends Component {
   render() {
     return (
-      <Text>ActorsMovies</Text>
+      <Text>Movies</Text>
     )
   }
 }
