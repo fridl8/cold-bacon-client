31fef27bde594404499f71c634baefb51c5d753a
Tyler Fridley <email": null>
2017-08-20 11:47:46 -0500

    Add beginning of startactorsmovies

---
+++ containers/StartingActorsMovies.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Text, Image, View } from 'react-native';
+import styles from './styles/ActorsMoviesStyle';
+import ActorImage from '../components/ActorImage';
+import { StackNagivator } from 'react-navigation';
+
+export default class StartingActorsMovies extends Component {
+  render() {
+    return (
+      <Text>Starting</Text>
+    )
+  }
+}
