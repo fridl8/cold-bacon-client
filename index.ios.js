4c98bb9add2f98cc4eb2bb33a2a601a62f051d95
Tyler Fridley <email": null>
2017-08-20 11:45:46 -0500

    Add startingactorsmovies to index

--- index.ios.js
+++ index.ios.js
@@ -4,11 +4,13 @@ import { StackNavigator } from 'react-navigation';
 import GameStart from './containers/GameStart';
 import LaunchScreen from './containers/LaunchScreen';
 import ActorsMovies from './containers/ActorsMovies';
+import StartingActorsMovies from './containers/StartingActorsMovies';
 
 const Nav = StackNavigator({
   LaunchScreen: {screen: LaunchScreen },
   GameStart: {screen: GameStart },
   ActorsMovies: {screen: ActorsMovies},
+  StartingActorsMovies: {screen: StartingActorsMovies},
   }, {
     headerMode: 'none'
   }
