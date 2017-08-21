f89e8260dd661b656ef390b34d21c64417a0ca7e
Tyler Fridley <email": null>
2017-08-21 09:25:49 -0500

    Add styles for clickablepath

---
+++ components/styles/ClickablePathStyle.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Dimensions } from 'react-native';
+
+export default StyleSheet.create({
+  touchableSize: {
+    width: 60,
+    alignItems: 'center',
+    // borderWidth: 1,
+  },
+  image: {
+    height: 65,
+    width: 65,
+    borderRadius: 32,
+  }
+});
