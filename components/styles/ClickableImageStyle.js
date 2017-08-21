c17d565465983c747305e8a0b75af7765bb68aba
Alan English <aenglish@manorhillcapital.com>
2017-08-21 13:41:09 -0500

    change styling slightly, keep path images same and look further into D3 or other alternative

--- components/styles/ClickableImageStyle.js
+++ components/styles/ClickableImageStyle.js
@@ -3,26 +3,18 @@ import { StyleSheet, Dimensions } from 'react-native';
 export default StyleSheet.create({
   touchableSize: {
     width: 50
-     // borderWidth:1,
-     // borderColor:'rgba(0,0,0,0.2)',
-     // alignItems:'center',
-     // justifyContent:'center',
-     // width:100,
-     // height:100,
-     // backgroundColor:'#fff',
-     // borderRadius:50,
   },
   image: {
     // height: 65,
     // width: 65,
     // borderRadius: 32,
-    borderWidth:1,
-    borderColor:'rgba(0,0,0,0.2)',
-    alignItems:'center',
-    justifyContent:'center',
-    width:100,
-    height:100,
-    backgroundColor:'#fff',
-    borderRadius:100,
+    // borderWidth:1,
+    // borderColor:'rgba(0,0,0,0.2)',
+    // alignItems:'center',
+    // justifyContent:'center',
+    // width:100,
+    // height:100,
+    // backgroundColor:'#fff',
+    // borderRadius:100,
   }
 });
