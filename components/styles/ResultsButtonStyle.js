3afb4f2f3e0a162f7bfbbe261136ee2fa8c73a15
Tyler Fridley <email": null>
2017-08-21 13:36:24 -0500

    Add styles for resultsbutton

---
+++ components/styles/ResultsButtonStyle.js
@@ -0,0 +1,17 @@
+import { StyleSheet, Dimensions } from 'react-native';
+
+export default StyleSheet.create({
+  button: {
+    height: 45,
+    borderRadius: 5,
+    marginHorizontal: 5,
+    backgroundColor: 'black',
+    justifyContent: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 15
+  }
+});
