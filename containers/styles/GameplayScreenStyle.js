ec99ed34603beeacb3478915a1b094536876c493
Tyler Fridley <email": null>
2017-08-21 07:39:13 -0500

    Add styles for views and images

--- containers/styles/GameplayScreenStyle.js
+++ containers/styles/GameplayScreenStyle.js
@@ -1,8 +1,22 @@
 import { StyleSheet, Dimensions } from 'react-native';
 
 export default StyleSheet.create({
+  mainContainer: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height
+  },
   image: {
-    height: 150,
-    width: 150
+    height: 80,
+    width: 80,
+    resizeMode: 'contain'
+  },
+  ActorView: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  endingPaths: {
+    marginTop: Dimensions.get('window').height / 14
   }
 })
