3869d8b2b670e2ed0aa5470d949b3b306622ce1d
Tyler Fridley <email": null>
2017-08-21 13:34:16 -0500

    Begin writing new tests and add compatables to package

--- __tests__/LaunchScreen.js
+++ __tests__/LaunchScreen.js
@@ -1,12 +1,13 @@
 import 'react-native';
 import React from 'react';
-import Index from '../index.android.js';
+import { StackNavigator } from 'react-navigation';
+import LaunchScreen from '../containers/LaunchScreen';
 
-// Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
   const tree = renderer.create(
-    <Index />
+    <LaunchScreen />
   ).toJSON();
-});
+  expect(tree).toMatchSnapshot();
+})
