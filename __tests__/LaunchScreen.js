15ea1dcab915fb26852bedf83e9bfad6fb66dd81
Tyler Fridley <email": null>
2017-08-22 07:56:17 -0500

    Change code to test testing

--- __tests__/LaunchScreen.js
+++ __tests__/LaunchScreen.js
@@ -1,13 +1,10 @@
 import 'react-native';
 import React from 'react';
-import { StackNavigator } from 'react-navigation';
 import LaunchScreen from '../containers/LaunchScreen';
 
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
-  const tree = renderer.create(
-    <LaunchScreen />
-  ).toJSON();
+  const tree = renderer.create(<LaunchScreen />).toJSON();
   expect(tree).toMatchSnapshot();
-})
+});
