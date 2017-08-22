16cb0348dab24ef26c98b4992aa2d04f73f4d525
Tyler Fridley <email": null>
2017-08-22 11:22:52 -0500

    Change clickableimage to allow it to take styles when a new component is called elsewhere

--- components/ClickableImage.js
+++ components/ClickableImage.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { TouchableOpacity, Image, View } from 'react-native';
-import styles from './styles/ClickableImageStyle';
+// import styles from './styles/ClickableImageStyle';
 import PropTypes from 'prop-types';
 import { StackNagivator } from 'react-navigation';
 import FadeInView from '../animations/FadeInView';
@@ -10,15 +10,17 @@ export default class ClickableImage extends Component {
     onPress: PropTypes.func,
     text: PropTypes.object,
     children: PropTypes.object,
-    navigation: PropTypes.object
+    navigation: PropTypes.object,
+    imageStyle: Image.propTypes.style,
+    touchStyle: View.propTypes.style
   }
 
   render() {
     return (
       <View>
         <FadeInView>
-          <TouchableOpacity style={styles.touchableSize} activeOpacity={.85} onPress={this.props.onPress}>
-              <Image style={styles.image} source={this.props.text} />
+          <TouchableOpacity style={this.props.touchStyle} activeOpacity={.85} onPress={this.props.onPress}>
+              <Image style={this.props.imageStyle} source={this.props.text} />
           </TouchableOpacity>
         </FadeInView>
       </View>
