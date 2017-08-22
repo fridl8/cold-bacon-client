b53f1ea8a67fd50b02b016f4ff205c12c87f98aa
Tyler Fridley <email": null>
2017-08-22 18:15:58 -0500

    update android index

--- index.android.js
+++ index.android.js
@@ -4,13 +4,17 @@ import { StackNavigator } from 'react-navigation';
 import GameStart from './containers/GameStart';
 import LaunchScreen from './containers/LaunchScreen';
 import GameplayScreen from './containers/GameplayScreen';
+import ResultsScreen from './containers/ResultsScreen';
 
 const Nav = StackNavigator({
-  LaunchScreen: {screen: LaunchScreen },
-  GameStart: {screen: GameStart },
+  LaunchScreen: { screen: LaunchScreen },
+  GameStart: { screen: GameStart },
   GameplayScreen: {
     path: 'games/:game_id/:traceable_id/:traceable_type',
-    screen: GameplayScreen},
+    screen: GameplayScreen },
+  ResultsScreen: {
+    path: 'games/:game_id',
+    screen: ResultsScreen },
   }, {
     headerMode: 'none'
   }
