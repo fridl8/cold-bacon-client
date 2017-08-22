48c0ee50e7c9e3d37fef5caf485546389cf77703
Tyler Fridley <email": null>
2017-08-22 11:23:19 -0500

    Add clickablepath styles to clickableimagestyle

--- components/styles/ClickableImageStyle.js
+++ components/styles/ClickableImageStyle.js
@@ -1,13 +1,12 @@
 import { StyleSheet, Dimensions } from 'react-native';
 
 export default StyleSheet.create({
-  touchableSize: {
+  startTouchable: {
     width: 50,
     justifyContent: 'center',
     alignItems: 'center',
   },
-
-  image: {
+  startImage: {
     borderWidth:1,
     borderColor:'#c0c0c0',
     alignItems:'center',
@@ -20,5 +19,24 @@ export default StyleSheet.create({
     borderRadius: 54,
     // resizeMode: 'cover',
     resizeMode: 'contain'
+  },
+  pathTouchable: {
+    width: 90,
+    height: 90,
+    alignItems: 'center',
+    justifyContent: 'center',
+    // borderWidth: 1,
+  },
+  pathImage: {
+    borderWidth:1,
+    borderColor:'#c0c0c0',
+    alignItems:'center',
+    justifyContent:'center',
+    width: 90,
+    height: 90,
+    backgroundColor:'black',
+    borderRadius: 45,
+    // resizeMode: 'contain',
+    resizeMode: 'cover'
   }
 });
