ea73504912dab89e4b2c1c6864c6a7aa934dcf52
Tyler Fridley <email": null>
2017-08-22 07:55:28 -0500

    Add setup file for jest

---
+++ __tests__/setup.js
@@ -0,0 +1,15 @@
+jest.mock('Linking', () => {
+
+  // we need to mock both Linking.getInitialURL()
+  // and Linking.getInitialURL().then()
+  const getInitialURL = jest.fn()
+  getInitialURL.mockReturnValueOnce({then: jest.fn()})
+
+  return {
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    openURL: jest.fn(),
+    canOpenURL: jest.fn(),
+    getInitialURL: getInitialURL
+  }
+})
