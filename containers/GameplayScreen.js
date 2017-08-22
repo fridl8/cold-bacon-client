b7684ba6f0ae3426c5ebcd4010023f7dfd2ae609
Alan English <aenglish@manorhillcapital.com>
2017-08-22 09:59:04 -0500

    change size of incoming images to speed up gameplay

--- containers/GameplayScreen.js
+++ containers/GameplayScreen.js
@@ -67,7 +67,7 @@ export default class GameplayScreen extends Component {
         <View style={styles.mainContainer}>
           <View style={styles.startingActorView}>
             <View>
-              <Image source={{uri: 'https://image.tmdb.org/t/p/original/'+this.state.pathInfo.current_traceable.traceable.image_url}} style={styles.actor_image} />
+              <Image source={{uri: 'https://image.tmdb.org/t/p/w185/'+this.state.pathInfo.current_traceable.traceable.image_url}} style={styles.actor_image} />
             </View>
           </View>
           <View style={styles.pathsView}>
@@ -75,7 +75,7 @@ export default class GameplayScreen extends Component {
               {
                 this.state.pathInfo.possible_paths.map(function(possible_path, index) {
                   return (
-                  <ClickablePath key={index} text={{uri: 'https://image.tmdb.org/t/p/w300/'+possible_path.traceable.image_url}} onPress={() => navigate('GameplayScreen', { game_id: responseObject.game_id, traceable_id: possible_path.traceable.id, traceable_type: possible_path.traceable_type} )} />
+                  <ClickablePath key={index} text={{uri: 'https://image.tmdb.org/t/p/w185/'+possible_path.traceable.image_url}} onPress={() => navigate('GameplayScreen', { game_id: responseObject.game_id, traceable_id: possible_path.traceable.id, traceable_type: possible_path.traceable_type} )} />
                   )
                 })
               }
@@ -83,7 +83,7 @@ export default class GameplayScreen extends Component {
           </View>
           <View style={styles.endingActorView}>
             <View>
-              <Image source={{uri: 'https://image.tmdb.org/t/p/original/'+this.state.pathInfo.ending_traceable.image_url}} style={styles.actor_image} />
+              <Image source={{uri: 'https://image.tmdb.org/t/p/w185/'+this.state.pathInfo.ending_traceable.image_url}} style={styles.actor_image} />
             </View>
           </View>
         </View>
