f07d11490d351db2ac0964325f367eae169e36ca
Tyler Fridley <email": null>
2017-08-25 07:08:59 -0500

    move actor/image styles from gameplayscreen

---
+++ components/styles/StaticImageStyle.js
@@ -0,0 +1,26 @@
+import { StyleSheet, Dimensions } from 'react-native';
+
+export default StyleSheet.create({
+  actor_image: {
+    borderWidth:1,
+    borderColor:'#c0c0c0',
+    alignItems:'center',
+    justifyContent:'center',
+    width: 160,
+    height: 160,
+    backgroundColor:'white',
+    borderRadius: 80,
+    resizeMode: 'cover'
+  },
+  movieImage: {
+    borderWidth:1,
+    borderColor:'#c0c0c0',
+    alignItems:'center',
+    justifyContent:'center',
+    width: 108.92,
+    height:160.38,
+    backgroundColor:'white',
+    borderRadius: 16.5,
+    resizeMode: 'contain'
+  },
+})
